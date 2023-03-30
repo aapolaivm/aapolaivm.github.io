c70edecea2722a6672346c7da5ffd72154d58bd4
Aapo Laivamaa <aapolai@uef.fi>
2023-03-31 01:32:33 +0300

    demoiluia

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.argv.includes('dev');
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
  // Consult https://github.com/sveltejs/svelte-preprocess
@@ -9,9 +11,10 @@ const config = {
  preprocess: preprocess(),
 
  kit: {
- // hydrate the <div id="svelte"> element in src/app.html
-  target: '#svelte',
   adapter: adapter({
+	paths: {
+		base: dev ? '' : '/aapolaivm.github.io/src/app.html'
+	},
     pages: 'build',
     assets: 'build',
     fallback: null
--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { svelte } from '@sveltejs/vite-plugin-svelte';
+import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [svelte()],
+	base: "/aapolaivm.github.io/",
+	plugins: [sveltekit()],
 	build: {
 		outDir: './docs', // relative to index.html
-    // emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
+     emptyOutDir: true, // true if outDir is inside root. if outDir is not inside root, uncomment this.
 	}
 });
